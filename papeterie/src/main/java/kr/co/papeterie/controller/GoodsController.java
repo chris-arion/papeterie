03858958447216a0340a58255d7bfcf6eb78f0c4
admin <admin@401PC-003>
2021-12-09 16:55:45 +0900

    [21/12/09] [sean1945] 	- main/java/ Goods관련 파일 추가 	- homecontroller goods 임시 삭제

---
+++ papeterie/src/main/java/kr/co/papeterie/controller/GoodsController.java
@@ -0,0 +1,21 @@
+package kr.co.papeterie.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.co.papeterie.service.GoodsService;
+
+@Controller
+@RequestMapping("/goods")
+public class GoodsController {
+	
+	@Autowired
+	@Qualifier("goods")
+	private GoodsService gservice;
+	
+	private final String module = "/goods";
+	
+	
+}
