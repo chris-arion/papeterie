bbf51aa6c3f68425f8d36bd97532c7452fac0054
admin <admin@401PC-013>
2021-12-10 18:09:15 +0900

    [2021.12.10][chris]   - 아이디 찾기/비밀번호 찾기 완료   - /member/mypage 추가

--- papeterie/src/main/java/kr/co/papeterie/controller/MemberController.java
+++ papeterie/src/main/java/kr/co/papeterie/controller/MemberController.java
@@ -1,5 +1,7 @@
 package kr.co.papeterie.controller;
 
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +18,7 @@ public class MemberController {
 	@Autowired
 	@Qualifier("member")
 	private MemberService service;
-	
+
 	@RequestMapping("/member/login")
 	public String login() {
 		return "/member/login";
@@ -37,6 +39,25 @@ public class MemberController {
 	public String mypage() {
 		return "/member/mypage";
 	}
-	
+
+	@RequestMapping("/member/userid_search_ok")
+	public void userid_search_ok(MemberVO mvo, PrintWriter out) {
+		String userid = service.userid_search_ok(mvo);
+		if (userid == null) {
+			out.print("0");
+		} else {
+			out.print(userid);
+		}
+	}
+
+	@RequestMapping("/member/pwd_search_ok")
+	public void pwd_search_ok(MemberVO mvo, PrintWriter out) throws Exception {
+		String pwd = service.pwd_search_ok(mvo);
+		if (pwd == null) {
+			out.print("0");
+		} else {
+			out.print(pwd);
+		}
+	}
 
 }
--- papeterie/src/main/java/kr/co/papeterie/service/MemberService.java
+++ papeterie/src/main/java/kr/co/papeterie/service/MemberService.java
@@ -6,4 +6,6 @@ import kr.co.papeterie.vo.MemberVO;
 
 public interface MemberService {
 	public String login_ok(MemberVO mvo, HttpSession session);
+	public String userid_search_ok(MemberVO mvo);
+	public String pwd_search_ok(MemberVO mvo);
 }
