ecc0e00f9603c586d6d54a7711a3d6f2671e3504
chris-arion <95663545+chris-arion@users.noreply.github.com>
2021-12-15 19:14:08 +0900

    [2021.12.15][chris]   - 일단 ok

--- papeterie/src/main/java/kr/co/papeterie/controller/MemberController.java
+++ papeterie/src/main/java/kr/co/papeterie/controller/MemberController.java
@@ -12,9 +12,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import kr.co.papeterie.service.AddressService;
+//import kr.co.papeterie.service.AddressService;
 import kr.co.papeterie.service.MemberService;
-import kr.co.papeterie.vo.AddressVO;
+//import kr.co.papeterie.vo.AddressVO;
 import kr.co.papeterie.vo.MemberVO;
 
 @Controller
@@ -23,11 +23,11 @@ public class MemberController {
 	@Autowired
 	@Qualifier("member")
 	private MemberService service;
-	
+/*	
 	@Autowired
 	@Qualifier("address")
 	private AddressService aservice;
-
+*/
 	@RequestMapping("/member/login")
 	public String login() {
 		return "/member/login";
@@ -68,7 +68,7 @@ public class MemberController {
 			out.print(pwd);
 		}
 	}
-
+/*
 	@RequestMapping("/member/addressbook")
 	public String addressbook(HttpSession session, Model model) {
 		return aservice.list(session, model);
@@ -96,6 +96,6 @@ public class MemberController {
 	public String del_addr(HttpServletRequest request) {
 		return aservice.del_addr(request);
 	}
-	
+*/	
 	
 }
